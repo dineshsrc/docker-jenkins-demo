66ef6b04c741aad14c3d0b62736fee0098b58ee6
DINESHKUMAR <DINESHKUMAR@LAPTOP-MGKNC0VL>
2020-03-28 23:03:14 +0530

    first commit

---
+++ docker-jenkins-demo/src/main/java/com/dp/docker_jenkins_demo/App.java
@@ -0,0 +1,13 @@
+package com.dp.docker_jenkins_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
